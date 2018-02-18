120b059d47884bf566bdeda693df6e9804f0c2a2
WINDOWS-AKBQBAF\Alenut <alexandru.nutu@mindit.io>
2018-02-18 11:22:36 +0200

    Test commit

--- src/main/java/com/alenut/mpi/resource/Users.java
+++ src/main/java/com/alenut/mpi/resource/Users.java
@@ -4,10 +4,10 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-//@Entity
-//@Table(name="user")
-//public class Users {
-//    @Id
-//
-//
-//}
+@Entity
+@Table(name="user")
+public class Users {
+    @Id
+
+
+}
