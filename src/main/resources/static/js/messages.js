b44f7da4ef1a1542994d8369a09e5f12009f22b8
Alenut <alexg.nutu@gmail.com>
2018-06-15 11:05:15 +0300

    small changes for messages

--- src/main/resources/static/js/messages.js
+++ src/main/resources/static/js/messages.js
@@ -1,16 +1,36 @@
-window.onload = function clickOnConversations(e) {
+$(document).ready(function () {
 
-    // Add active class to the current conversation (highlight it)
-    // var header = document.getElementById("allConversations");
-    // var conversations = header.getElementsByClassName("contact");
-    // for (var i = 0; i < conversations.length; i++) {
-    //     conversations[i].addEventListener("click", function() {
-    //         var current = document.getElementsByClassName("contact");
-    //         for(var j = 0; j < current.length; j++){
-    //             current[j].className = current[j].className.replace(" active", "");
-    //         }
-    //         this.className += " active";
+    // $("[data-toggle=tooltip]").tooltip();
+
+    $('.sBtn').on('click', function (event){
+        event.preventDefault();
+        var href = $(this).attr('href');
+        $.get(href, function(user, status){
+            $('.myForm #send_date').val(user.full_name);
+            $('.myForm #id_receiver').val(user.id);
+        });
+        $('.myForm #exampleModal').modal();
+    });
+
+    // $('.details .delBtn').on('click', function (event){
+    //     event.preventDefault();
+    //     var href = $(this).attr('href');
+    //     $.get(href, function(idea, status){
+    //         $('.myFormDelete #myModal #id').val(idea.id);
     //     });
-    // }
+    //     // $('#myModal #delRef').attr('href', href);
+    //     $('.myFormDelete #myModal').modal();
+    // });
 
-}
+    // $('.myFormDelete #deleteMyIdeaForm').on('submit', function(e) {
+    // trebuie adaugata atributul  dismiss de la butonul de No, pus pe un element din form
+    // id-ul ideii, dupa care obtinute toate ideile din div-ul ce contine lista ideilor si stearsa
+    // cea careia ii corespunde id-ul, apoi trigger-uita notificarea ca s-a sters ideea :)
+    //     e.preventDefault(); // prevent native submit
+    //
+    //     $(this).ajaxSubmit({
+    //         target: 'myResultsDiv'
+    //     });
+    // });
+
+});
